6f83d3cc90269406b71b767cb5ae214dd211b2a0
Linghanchujian <2739069093@qq.com>
2022-09-03 18:12:21 +0800

    Delete typings directory

--- typings/Live2d/lAppPal.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class LAppPal {
-    private static _deltaTime;
-    private static _currentFrame;
-    private static _lastFrame;
-    static lastUpdate: number;
-    static printMessage(msg: string): void;
-    static getDeltaTime(): number;
-    static updateTime(): void;
-}
